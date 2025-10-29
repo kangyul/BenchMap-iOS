8902aed560c66ee92d9e10203b0bcb8982eeedec
Yul Kang <yulkang2019@gmail.com>
2025-10-29 12:05:34 +0900

    Initial Commit

---
+++ BenchMap/BenchMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchMapApp.swift
+//  BenchMap
+//
+//  Created by Yul Kang on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BenchMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
